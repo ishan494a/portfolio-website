2de8e58fd3a011f3909f8526dd1223d07e678559
Ishan Jain <ishanjain2000@gmail.com>
2025-04-01 01:58:37 -0400

    fixed eslint issue in project.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ export default function Projects() {
         <section id="projects" className="min-h-screen flex items-center justify-center bg-black-900">
         <div className="text-center">
           <h2 className="text-3xl font-bold mb-4">Projects</h2>
-          <p className="text-lg">Here are some of the projects I've worked on...</p>
+          <p className="text-lg">Here are some of the projects I have worked on...</p>
           {/* Add project details here */}
         </div>
       </section>

3ac7fa209956e27530436f91454f4aab9c77c454
Ishan Jain <ishanjain2000@gmail.com>
2025-04-01 03:01:59 -0400

    eslint fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
-import { FaExternalLinkAlt } from 'react-icons/fa';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
